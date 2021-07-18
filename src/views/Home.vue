b3119622f9195ba438c93dd32668c9ee95153cd1
daisuke14012719 <daiyama0127@outlook.jp>
2021-07-18 10:47:36 +0900

    first-commit

---
+++ src/views/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="home flex">
+    <Card v-for="(data, index) in lists" :key="index" :city="data.city" />
+  </div>
+</template>
+
+<script>
+import Card from "../components/Card";
+export default {
+  data() {
+    return {
+      lists: [
+        {
+          city: "sapporo"
+        },
+        {
+          city: "tokyo"
+        },
+        {
+          city: "yokohama"
+        },
+        {
+          city: "nagoya"
+        },
+        {
+          city: "osaka"
+        },
+        {
+          city: "kobe"
+        },
+        {
+          city: "hiroshima"
+        },
+        {
+          city: "fukuoka"
+        },
+        {
+          city: "naha"
+        }
+      ]
+    };
+  },
+  components: {
+    Card
+  }
+};
+</script>
+
+<style scoped>
+body {
+  background: #eee;
+}
+.flex {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+</style>
